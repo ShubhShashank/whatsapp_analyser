1d273944a74630b83800a8c860b1661849d70445
wsqsc735 <wsqsc735@gmail.com>
2016-06-21 21:21:14 +0100

    setup.py corrected

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 setup(
   name = 'whatsapp_analyser',
   packages = ['whatsapp_analyser'], 
-  version = '0.98',
+  version = '0.99',
   description = 'A package for the analysis of whatsapp messages',
   author = 'wsqsc735',
   author_email = 'wsqsc735@gmail.com',
   license='MIT',
-  install_requires=['numpy', 'csv','string','pandas','matplotlib'],
+  install_requires=['pandas'],
   url='https://github.com/wsqsc735/whatsapp_analyser', 
-  download_url = 'https://github.com/wsqsc735/whatsapp_analyser/tarball/0.98', 
-  keywords = ['whatsapp', 'analysis', 'messages','android','message','data'], 
+  download_url = 'https://github.com/wsqsc735/whatsapp_analyser/tarball/0.99', 
+  keywords = ['whatsapp', 'analysis', 'messages','android', 'message','data'], 
   classifiers = [],
 )
