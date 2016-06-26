aa1d2c46d89cf26ea1cbc78cbb820157b2a77a0e
wsqsc735 <wsqsc735@gmail.com>
2016-06-26 12:45:23 +0100

    changed setup

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 setup(
   name = 'whatsapp_analyser',
   packages = ['whatsapp_analyser'], 
-  version = '1.01',
+  version = '1.2',
   description = 'A package for the analysis of whatsapp messages',
   author = 'wsqsc735',
   author_email = 'wsqsc735@gmail.com',
   license='MIT',
   install_requires=['pandas'],
   url='https://github.com/wsqsc735/whatsapp_analyser', 
-  download_url = 'https://github.com/wsqsc735/whatsapp_analyser/tarball/1.01', 
+  download_url = 'https://github.com/wsqsc735/whatsapp_analyser/tarball/1.2', 
   keywords = ['whatsapp', 'analysis', 'messages','android', 'message','data'], 
   classifiers = [],
 )
